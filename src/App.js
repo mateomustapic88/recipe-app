85b78077d9602743da831aeb0b4dd28d2b282e98
mateo.mustapic <mateo.mustapic88@gmail.com>
2021-01-26 17:08:19 +0100

    commit recipe-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import Recipe from './Recipe';
+import React, { useEffect, useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const APP_ID = '206522ba';
+	const APP_KEY = 'a01399a6af58a37cf4dc415e03da15d6';
+
+	const [recipes, setRecipes] = useState([]);
+
+	const [search, setSearch] = useState('');
+
+	const [query, setQuery] = useState('chicken');
+
+	const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+	useEffect(() => {
+		getRecipes();
+	}, [query]);
+
+	const getRecipes = async () => {
+		const response = await fetch(url);
+		const data = await response.json();
+		setRecipes(data.hits);
+	};
+
+	const updateSearch = (e) => {
+		setSearch(e.target.value);
+		console.log(search);
+	};
+
+	const getSearch = (e) => {
+		e.preventDefault();
+		setQuery(search);
+		setSearch('');
+	};
+
+	return (
+		<div className='App'>
+			<form onSubmit={getSearch} className='search-form'>
+				<input className='search-bar' type='text' />
+				<button
+					className='search-button'
+					type='submit'
+					value={search}
+					onChange={updateSearch}
+				>
+					Search
+				</button>
+			</form>
+			<div className='recipes'>
+				{recipes.map((recipe) => (
+					<Recipe
+						key={recipe.recipe.label}
+						title={recipe.recipe.label}
+						calories={recipe.recipe.calories}
+						image={recipe.recipe.image}
+						ingredients={recipe.recipe.ingredients}
+					/>
+				))}
+			</div>
+		</div>
+	);
+};
 
 export default App;
